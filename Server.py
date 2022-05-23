9ec4ab7da276b84a66400a511778e71a366000f9
simonkaemmer <simon.kaemmer@yahoo.de>
2022-05-23 21:19:26 +0200

    Added first functions

---
+++ Server.py
@@ -0,0 +1,13 @@
+class Chat_Server:
+
+    def register(self, nick, ip, port):
+
+    def getUserList(self):
+
+    def updateClients(self):
+
+    def registerBroadcast(self):
+
+    def broadcast(self):
+
+
