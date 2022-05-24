7e8aea4734290ba8fe1d61402367450c1004d25a
Simon <Simon.Kaemmer@yahoo.de>
2022-05-24 16:43:03 +0200

    Start of Serverimplementation

--- Server.py
+++ Server.py
@@ -1,13 +1,45 @@
-class Chat_Server:
+import socket
+import threading
+from _thread import *
+from struct import *
 
-    def register(self, nick, ip, port):
-
-    def getUserList(self):
-
-    def updateClients(self):
-
-    def registerBroadcast(self):
-
-    def broadcast(self):
+nickname_len = 10
+userlist_len = 10
+format_strings = ['b b s I I', '', '']
+f_string = 'b b s I I'  # Byte, Byte, string mit länge i, UnsignedInt, UnsignedInt
+print_lock = threading.Lock()
 
 
+# Fragen: Max len of nickanme?
+# Byte-Anzahlen für die jeweiligen längen?
+
+def thread(c):
+    while True:
+
+        data = c.recv(1024)
+        if not data:
+            print_lock.release()
+            break
+        data = get_data(data)
+
+
+def get_data(bytedata, action):
+    n = unpack(format_strings[action], bytedata)
+    n =
+
+
+def Main():
+    host = ""
+    port = 5000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+
+    s.listen(5)
+    print("Listening...")
+
+    while True:
+        c, addr = s.accept()
+
+        print_lock.acquire(timeout=10.0)
+        print("Connected to : ", addr[0], ":", addr[1])
+        start_new_thread(thread, (c,))
